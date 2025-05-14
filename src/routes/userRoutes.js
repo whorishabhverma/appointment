e365b85a3ef17ad3b5c13f6c810e0fa717d932cb
rishabh verma <2021aspire28@gmail.com>
2025-05-14 10:26:55 +0530

    Revert "added server.js and install packages"
    This reverts commit ad7a6d2598aa0d28234c6cd82f181a996ab203fe.

--- src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// Sample test route
-router.get("/test", (req, res) => {
-  res.json({ message: "User route is working!" });
-});
-
-module.exports = router;
